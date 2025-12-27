bd6b295771059c49f82431851314d520bf255985
Yamini-519 <24331a05i9@mvgrce.edu.in>
2025-12-27 21:25:21 +0530

    Add files via upload

---
+++ maxmin.py
@@ -0,0 +1,20 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=int(input("enter c value:"))
+print("maximum of three values:")
+#maximum
+if a>b and a>c:
+    print("a is maximum")
+elif b>a and b>c:
+    print("b is maximum")
+else:
+    print("c is maximum")  
+#minimum
+if a<b and a<c:
+    print("a is minimum")
+elif b<a and b<c:
+    print("b is minimum")
+else:
+    print("c is minimum")  
+         
+
